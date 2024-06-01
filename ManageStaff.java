ef5b5934edc1990d9702b88384b1d765f4cd9f46
Gxvdon4601 <127635294+Gxvdon4601@users.noreply.github.com>
2024-06-02 00:27:45 +1000

    Add files via upload

--- ManageStaff.java
+++ ManageStaff.java
@@ -4,9 +4,6 @@
  */
 package com.cqu.aise.server;
 
-import com.cqu.aise.server.Recruit;
-import com.cqu.aise.server.Staff;
-
 /**
  *
  * @author 61475
@@ -16,14 +13,23 @@ public class ManageStaff extends Staff {
     private int manageStaffId;
     private String managementLevel;
 
-    public ManageStaff(String userType, String userName, String password, String fullName, String address, String phone, String email, String location) {
+    public ManageStaff(String managementLevel, String userType, String userName, String password, String fullName, String address, String phone, String email, String location) {
         super(userType, userName, password, fullName, address, phone, email, location);
+        this.managementLevel = managementLevel;
     }
 
-    public void manageRegister() {
-        super.userRegister(); 
-
-        System.out.println("Recruit registered");
+    @Override
+    public void register() {
+        DatabaseUtils.manageRegister(this);
+        System.out.println("ManagementStaff data saved to LinkedList");
+        System.out.println("User Type: " + getUserType());
+        System.out.println("Username: " + getUserName());
+        System.out.println("Full Name: " + getFullName());
+        System.out.println("Address: " + getAddress());
+        System.out.println("Phone: " + getPhone());
+        System.out.println("Email: " + getEmail());
+        System.out.println("Location: " + getLocation());
+        System.out.println("ManagementLevel: " + managementLevel);
     }
 
     public void assignDepartment(Recruit recruit, String department) {
@@ -36,4 +42,13 @@ public class ManageStaff extends Staff {
 
     // Getters and setters
     // toString() method
+    public String getManagementLevel() {
+        return managementLevel;
+    }
+
+    public void setManagementLevel(String managementLevel) {
+        this.managementLevel = managementLevel;
+    }
+    
+    
 }
